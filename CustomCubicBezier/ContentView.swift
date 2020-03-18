a545af55db22a1c014a86a62c38321ed93f295b9
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-03-18 12:42:40 +0200

    init

--- CustomCubicBezier/ContentView.swift
+++ CustomCubicBezier/ContentView.swift
@@ -9,8 +9,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var locked = false
+    @State private var tilted = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Rectangle()
+                .frame(width: 64, height: 64)
+                .cornerRadius(6)
+                .foregroundColor(Color.orange)
+            
+            Capsule()
+                .trim(from: locked ? 0 : 3/5, to: 1)
+                .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+                .frame(width: 44, height: 74)
+                .foregroundColor(Color.orange)
+                .offset(y: -30)
+                .animation(Animation.easeInOut.delay(0.5).repeatCount(1, autoreverses: true))
+                .onAppear() {
+                    self.locked.toggle()
+            }
+        }.rotationEffect(.degrees(tilted ? 0 : 30))
+            .animation(Animation.easeInOut.delay(0.5).repeatCount(1, autoreverses: true))
+            .onAppear() {
+                self.tilted.toggle()
+        }.onTapGesture {
+            self.locked.toggle()
+            self.tilted.toggle()
+        }
     }
 }
 
